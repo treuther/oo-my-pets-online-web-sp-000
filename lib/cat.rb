d78424135fd497677c4affe61a6780f64b43c7bb
Tracy Reuther <tracyreuther@gmail.com>
2019-07-03 21:14:40 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -20,11 +20,9 @@ class Cat
     
   end
   
-  attr_reader :name
-
-  def initialize(name, mood = "nervous")
-    @name = name
-    @mood = mood
-  end
+  # def initialize(name, mood = "nervous")
+  #   @name = name
+  #   @mood = mood
+  # end
 
 end
