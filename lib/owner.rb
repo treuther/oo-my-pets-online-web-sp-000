2dccd0e2236adec6f77aa021145d1ab8d7964ff8
Tracy Reuther <tracyreuther@gmail.com>
2019-07-03 22:43:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,11 +61,10 @@ def walk_dogs
 def sell_pets
   @dogs.collect do |dog|
     dog.mood = "nervous"
-    dog.owner.clear
+
   end
   @cats.collect do |cat|
     cat.mood = "nervous"
-    cat.owner.clear
   end
 end
     
